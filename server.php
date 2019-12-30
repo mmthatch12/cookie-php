6b9568ecd27409a74e004e83ff062ccc99145572
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-30 15:01:34 -0700

    finished server

--- server.php
+++ server.php
@@ -1,10 +1,23 @@
 <!DOCTYPE>
 <html>
     <head>
-        <title>Setting Cookie</title>
+        <title>Server</title>
     </head>
     <body>
 <?php 
+echo "Server Details-><br>";
+    echo "SERVER_NAME->".$_SERVER['SERVER_NAME']."<br>";
+    echo "SERVER_ADDR->".$_SERVER['SERVER_ADDR']."<br>";
+    echo "SERVER_PORT->".$_SERVER['SERVER_PORT']."<br>";
+    echo "DOCUMENT_ROOT->".$_SERVER['DOCUMENT_ROOT']."<br>";
+    echo "<br>";
+echo "Page Details-><br>";
+    echo "PHP_SELF-> ".$_SERVER['PHP_SELF'] ."<br>";
+    echo "SCRIPT_FILENAME-> ".$_SERVER['SCRIPT_FILENAME'] ."<br>";
+    echo "<br>";
+    //there are a ton more properties that can be accessed with server
+
+
 
 ?>
 
